7746397962df572ea63df93232d685daee9879db
pritom-dey1 <Pritom1.2.zx@gmail.com>
2025-12-27 22:17:29 +0600

    added force to submit

--- app/(dashboard)/client/feedbacks/submit/page.jsx
+++ app/(dashboard)/client/feedbacks/submit/page.jsx
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = 'force-dynamic';
+
 import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import api from "@/lib/axios";
